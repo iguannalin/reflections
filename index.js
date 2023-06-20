998bc043765205e771c24214bdd90dc75d25a29e
seasons986 <annalin12345@gmail.com>
2023-06-20 09:57:10 -0400

    :)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let text = "Anna Y Lin is a Cantonese-American artist who is currently pursuing a masters at NYU's ITP. She studied psychology and, initially, art--which led her to a love of computer science. She's worked in multiple fields as a UX engineer in the Bay Area and Boston, and now works on projects that investigates via introspection our relationship to technology and the digital world through art.";
-
+let speed = 10;
 window.addEventListener("load", () => {
   const left = document.getElementById("left");
   const right = document.getElementById("right");
@@ -10,7 +10,8 @@ window.addEventListener("load", () => {
     spaced.forEach((letter) => {
       setTimeout(async () => {
         right.innerHTML += letter;
-      }, timer += 10);
+        left.innerHTML += letter;
+      }, timer += speed);
     });
   });
 });
