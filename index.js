2a96d4e41e1281647d4a0fe98590435dc54cedb3
seasons986 <annalin12345@gmail.com>
2023-06-20 23:20:52 -0400

    nice

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let timer = 100;
 let speed = 100;
+let first = true;
+let left;
+let right;
 let timeout;
+let w;
+let h;
 
 let debounce = function(func, delay) {
   clearTimeout(timeout);
@@ -9,6 +14,7 @@ let debounce = function(func, delay) {
 
 function onType(e) {
   if (!e || !e.data) return;
+  if (e.target.value.length < 2) newTranslate();
   right.innerHTML += e.data;
   if (e.data.match(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g)) left.innerHTML += e.data;
   else debounce(() => translate(e.target.value), 250);
@@ -24,9 +30,29 @@ async function translate(q) {
   if (data) left.innerHTML = data;
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function newTranslate() {
+  if (first) {
+    first = false;
+    return;
+  }
+  let elem = document.createElement('div');
+  elem.className = "left";
+  console.log(Math.floor(Math.random(0,w)));
+  elem.style.left = `${getRandomInt(w)}px`;
+  elem.style.top = `${getRandomInt(h)}px`;
+  document.body.appendChild(elem);
+  left = elem;
+}
+
 window.addEventListener("load", () => {
-  const left = document.getElementById("left");
-  const right = document.getElementById("right");
+  left = document.getElementById("left");
+  right = document.getElementById("right");
   let input = document.getElementById("prompt");
   input.oninput = onType;
+  w = window.innerWidth - 100;
+  h = window.innerHeight - 200;
 });
